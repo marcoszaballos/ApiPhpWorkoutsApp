3b650b75cb0fb63d7ea43b1e418e7bebf3c7eac5
Marcos <marcoszaballos@gmail.com>
2023-09-28 22:47:05 +0200

    Se elimina código comentado y se retorna Array

--- getListaGruposMusculares.php
+++ getListaGruposMusculares.php
@@ -1,29 +1,15 @@
 <?php
-/*if($_SERVER["REQUEST_METHOD"]=="GET"){
-    require_once 'conexion.php';
-    $query="SELECT * FROM DM_GRUPOS_MUSCULARES";
-    $resultado=$mysql->query($query);
-    if($mysql->affected_rows > 0){
-        $array;
-        while($row=$resultado->fetch_assoc()){
-            echo json_encode($row);
-        }
-    }else{
-        echo "No hay registros";
-    }
-    $resultado->close();
-    $mysql->close();
-}*/
 require_once 'conexion.php';
 // FETCH_OBJ
 $stmt = $mysql->prepare("SELECT * FROM DM_GRUPOS_MUSCULARES");
 // Ejecutamos
 $stmt->execute();
+
+$resultArray = array();
+
 // Ahora vamos a indicar el fetch mode cuando llamamos a fetch:
-while($row = $stmt->fetch(PDO::FETCH_OBJ)){
-    echo "ID: " . $row->ID . "<br>";
-    echo "NOMBRE: " . $row->NOMBRE . "<br><br>";
-    
-    //echo json_encode($row);
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $resultArray[] = $row;
 }
+echo json_encode($resultArray);
 ?>

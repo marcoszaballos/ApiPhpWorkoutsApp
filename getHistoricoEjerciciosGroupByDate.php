56835732124e6a568f5be3c99b742ae396f37ac5
marcoszaballos <44778619+marcoszaballos@users.noreply.github.com>
2024-05-05 22:44:32 +0200

    Create getHistoricoEjerciciosGroupByDate.php

---
+++ getHistoricoEjerciciosGroupByDate.php
@@ -0,0 +1,21 @@
+<?php
+//Permitir solicitudes desde cualquier origen:
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: *");
+
+require_once 'conexion.php';
+$userEmail=$_GET['userEmail'];
+// FETCH_OBJ
+$stmt = $mysql->prepare("SELECT DATE_FORMAT(ULTIMO_ENTRENO, '%d/%m/%Y') AS ULTIMO_ENTRENO FROM TB_HISTORICO_EJERCICIOS WHERE USUARIO = '".$userEmail."' GROUP BY DATE_FORMAT(ULTIMO_ENTRENO, '%d/%m/%Y') ORDER BY ULTIMO_ENTRENO DESC");
+// Ejecutamos
+$stmt->execute();
+
+$resultArray = array();
+
+// Ahora vamos a indicar el fetch mode cuando llamamos a fetch:
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $resultArray[] = $row;
+}
+echo json_encode($resultArray);
+?>
